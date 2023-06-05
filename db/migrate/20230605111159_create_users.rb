7fdb8dc1bdb7857726b11558f02c98a02b1555d8
kashif tufail <kashif.tufail17@gmail.com>
2023-06-05 16:20:51 +0500

    add model counts for counter caches

--- db/migrate/20230605111159_create_users.rb
+++ db/migrate/20230605111159_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :email
       t.string :name
+      t.integer :books_count
 
       t.timestamps
     end
--- db/migrate/20230605111302_create_books.rb
+++ db/migrate/20230605111302_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
     create_table :books do |t|
       t.string :title
       t.text :description
+      t.integer :essays_count
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
