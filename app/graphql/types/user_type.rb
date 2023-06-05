fbb1d40d0b4fbcda9dcea35d82c87b24b9f82b0d
kashif tufail <kashif.tufail17@gmail.com>
2023-06-05 18:25:42 +0500

    add user type

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String
+    field :name, String    
+    field :books_count, Integer
+    field :email_and_name, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false    
+
+    def email_and_name
+      object.email + " " + object.name
+    end
+  end
+end
+
+
+    
+  
