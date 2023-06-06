611f46a8b810066be9cb5f8ab7f69faf86175027
kashif tufail <kashif.tufail17@gmail.com>
2023-06-06 16:18:28 +0500

    add current user to session for use

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -10,7 +10,9 @@ class GraphqlController < ApplicationController
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
-      # current_user: current_user,
+      session: session,
+      current_user: current_user
+
     }
     result = GraphqlNestedSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
@@ -20,6 +22,20 @@ class GraphqlController < ApplicationController
   end
 
   private
+
+  # get current user from token stored in the session
+  def current_user
+    
+    return unless session[:token]
+
+    crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+    token = crypt.decrypt_and_verify session[:token]
+    id = token.gsub('user-id:', '').to_i
+    User.find id
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    nil
+  end
+
 
   # Handle variables in form data, JSON body, or a blank value
   def prepare_variables(variables_param)
--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -25,6 +25,7 @@ module Mutations
          # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
          crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
          token = crypt.encrypt_and_sign("user-id:#{ user.id }")
+         context[:session][:token] = token
          { user: user, token: token, message: 'ok' }
        else        
          { message: 'password is invalid' }
