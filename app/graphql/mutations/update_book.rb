80051ce6c09085dbb57f55b5414bb9625aa31a69
kashif tufail <kashif.tufail17@gmail.com>
2023-06-07 14:24:30 +0500

    user can only edit its own book

--- app/graphql/mutations/update_book.rb
+++ app/graphql/mutations/update_book.rb
@@ -2,20 +2,22 @@ module Mutations
   class UpdateBook < BaseMutation
     description "Updates Book"
 
-    argument :name, String, required: true
+    argument :title, String, required: true
+    argument :description, String, required: true
     argument :id, ID, required: true
 
-    field :user, Types::UserType, null: false
-    field :errors, [String], null: false
+    field :book, Types::BookType, null: true
+    field :errors, [String], null: true
 
     def resolve(title:, description:, id:)
-      book = Book.find(id)
-      book.update_attributes(title: title, description: description)
-
-      if(book.errors.blank?)
-        {book: book, errors: nil}
-      else
-        {author: nil, errors: book.errors.full_messages}
+      book = Book.find(id)       
+      return if book.user != context[:current_user]               
+      
+      book.update(title: title, description: description)        
+      if (book.errors.blank?)
+        { book: book, errors: nil }
+      else        
+        { book: nil, errors: book.errors.full_messages }
       end
     end
 
